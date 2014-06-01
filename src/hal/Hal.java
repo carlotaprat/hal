5bbf28f9ab04d3efc6b332bb40ebaf052ddfb12c
Héctor <hector0193@gmail.com>
2014-06-01 13:15:18 +0200

    Starting interpreter section.

--- src/hal/Hal.java
+++ src/hal/Hal.java
@@ -29,7 +29,7 @@ import java.io.PrintWriter;
 public class Hal
 {
     public static final String VERSION = "0.0.3@$GIT";
-    public static final String DATE = "$DATE";
+    public static final String DATE = "2014 jun 01, 11:41";
 
     public static Interpreter INTERPRETER = null;
     /** The file name of the program. */
