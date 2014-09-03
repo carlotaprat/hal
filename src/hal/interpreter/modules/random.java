bbeab20a807a412964ebc67b4d0021e415bfd87b
Héctor Ramón Jiménez <hector0193@gmail.com>
2014-09-03 02:21:25 +0200

    random module to get random integers

--- src/hal/interpreter/modules/random.java
+++ src/hal/interpreter/modules/random.java
@@ -1,14 +1,31 @@
 package hal.interpreter.modules;
 
+import hal.interpreter.core.Arguments;
+import hal.interpreter.core.Builtin;
+import hal.interpreter.core.ReferenceRecord;
+import hal.interpreter.types.HalMethod;
 import hal.interpreter.types.HalModule;
+import hal.interpreter.types.HalObject;
 import hal.interpreter.types.HalPackage;
 import hal.interpreter.types.numeric.HalInteger;
 
+import java.util.Random;
+
 
 public class random extends HalModule {
+    private Random generator;
+
     public random(HalPackage pkg) {
         super("random", pkg);
 
-        getInstanceRecord().defineVariable("magic", new HalInteger(42));
+        generator = new Random(System.currentTimeMillis());
+        ReferenceRecord module = getInstanceRecord();
+
+        module.defineMethod(new Builtin("integer") {
+            @Override
+            public HalObject call(HalObject instance, HalMethod lambda, Arguments args) {
+                return new HalInteger(generator.nextInt());
+            }
+        });
     }
 }
